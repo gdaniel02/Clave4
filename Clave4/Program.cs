e3083a7601dbfe98897d9fd32240dbf23b12ea3b
Daniel López <gdaniellopez02@gmail.com>
2025-11-20 20:48:51 -0600

    Primer avance

--- Clave4/Program.cs
+++ Clave4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Variables
+        string nombreCliente, nombreEmpleado, matricula;
+        double precioTotalSinDescuento, descuentoAplicado, totalConDescuento, MontoTotal;
+        int opcion, contador = 0, posicion = -1;
+
+        string[] Servicio = new string[7] {"Lavado exterior basico $30", "Lavado completo (interior y exterior) $55",
+        "Encerado $70", "Detallado de motor $90", "Pulido de carroceria $120", "Sanitizacion interior $50", "Limpieza profunda tapiceria $85"};
+
+        string[] ServiciosAdquiridos = new string[7];
+
+        string[] empleados = new string[3] { "Nilzon Anzora", "Diego Henriquez", "Daniel Lopez"};
+
+        double[] ventasEmpleados = new double[3];
+
+        double[] ventasEmpleadosSinDescuento = new double[3];
+
+        double[] comisiones = new double[3];
+
+        double[] porcentajeEmpleado = new double[3];
+
 
     }
 }
